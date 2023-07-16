d0f7de91f46153e8089742123e48faa8d6d0c988
Andar Sitohang <andarsitohang@gmail.com>
2023-07-16 17:44:05 +1000

    day-001

---
+++ Training/02. 100 Days of Code/Day-001_Working with Variables in Python to Manage Data/day-001.py
@@ -0,0 +1,37 @@
+print("Hello Andar \n")
+
+# Exercise 1 - Printing
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("\n")
+
+# Exercise 2 - Debugging Practice
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+print("\n")
+
+# Add space between to strings
+print("Hello" + "Angela")
+print("Hello" + " " + "Angela")
+print("\n")
+
+# Exercise 3 - Input Function
+print(len(input("What is your name? ")))
+
+# another mode (nested string)
+print("Hello " + input("What is your name ? "))
+print("\n")
+
+# Exercise 4 - Variables
+a = input("a : ")
+b = input("b : ")
+
+temp = a
+a = b
+b = temp
+
+print("a: " + a)
+print("b: " + b)
